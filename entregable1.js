4e341ca1a9fa678a77ac6d26ea242b2f75acc4d7
Fernando Rios <fernandorios.sf@gmail.com>
2022-08-23 00:38:59 -0300

    Incorporando funciones

--- entregable1.js
+++ entregable1.js
@@ -11,6 +11,22 @@ let descuento2 = 0.30;
 let precio1Oferta = ((precio1)-(precio1*descuento1));
 let precio2Oferta = ((precio2)-(precio2*descuento2));
 
+function oferta1 (precio1, descuento1) {
+    precio1OfertaFunction = (precio1)-(precio1*descuento1);
+}
+
+function oferta2 (precio2, descuento2) {
+    precio2OfertaFunction = (precio2)-(precio2*descuento2);
+}
+
+function descuento1Function (precio1, descuento1) {
+    descuentoObtenido1 = precio1 * descuento1;
+}
+
+function descuento2Function (precio2, descuento2) {
+    descuentoObtenido2 = precio2 * descuento2;
+}
+
 alert("Los productos que usted quiere comprar suman" + " " + "AR$"+(precio1Oferta+precio2Oferta) + ".");
 
 alert(" Está ahorrando" + " " + "AR$"+((precio1*descuento1)+(precio2*descuento2)));
@@ -18,22 +34,25 @@ alert(" Está ahorrando" + " " + "AR$"+((precio1*descuento1)+(precio2*descuento2
 
 let otraConsulta = prompt("¿Desea consultar otros productos? Escriba 'S' y Enter o Aceptar para continuar o culaquier tecla para cerrar")
 
+
+
 if (otraConsulta == "S") {
     precio1 = prompt("Ingrese el precio del primer producto");
     precio2 = prompt("Ingrese el precio del segundo producto");
 
-    descuento1 = 0.20;
-    descuento2 = 0.30;
+    oferta1 (precio1, descuento1);
+    oferta2 (precio2, descuento2);
+    descuento1Function (precio1, descuento1);
+    descuento2Function (precio2, descuento2);
 
-    precio1Oferta = ((precio1)-(precio1*descuento1));
-    precio2Oferta = ((precio2)-(precio2*descuento2));
+    alert("Los productos que usted quiere comprar suman" + " " + "AR$"+(oferta1 + oferta2) + ".");
 
-    alert("Los productos que usted quiere comprar suman" + " " + "AR$"+(precio1Oferta+precio2Oferta) + ".");
-
-    alert(" Está ahorrando" + " " + "AR$"+((precio1*descuento1)+(precio2*descuento2)));
-
-    otraConsulta = prompt("¿Desea consultar otros productos? Escriba 'S' y Enter o Aceptar para continuar o culaquier tecla para cerrar")
-
+    alert(" Está ahorrando" + " " + "AR$"+(descuento1 + descuento2));
+    
+    
 } else {
     alert("Muchas gracias por utilizar nuestro servicio. ¡Que tenga un lindo día!")
-}
+    }
+
+
+otraConsulta = prompt("¿Desea consultar otros productos? Escriba 'S' y Enter o Aceptar para continuar o culaquier tecla para cerrar")
