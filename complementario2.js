543479efd05757e9223acca3db5d815e57654c09
Fernando Rios <fernandorios.sf@gmail.com>
2022-08-31 23:45:16 -0300

    Correccion de sintaxis if else y mejor a de experiencia usuario en alerts de seccion en construccion

--- complementario2.js
+++ complementario2.js
@@ -5,25 +5,35 @@
 let tipoUsuario = prompt("Bienvenido! Ingresa la letra A mayúscula si eres Administrador o la letra C mayúscula si eres Cliente");
 
 
-
 if (tipoUsuario == "A") {
+    
     alert("¡Hola Adminisrador! A continuación, selecciona el número correspondiente a la acción que deseas realizar");
         
     let accionesAdmin = prompt("1 Buscar Producto, 2 Buscar Marca, 3 Agregar Producto, 4 Agregar Marca");
 
     if (accionesAdmin == 1) {
         
-    } else {
-        if (accionesAdmin == 2) {
+        alert("Sección en construcción. ¡Disculpa las molestias! Por favor, refresca el navegador para reiniciar el proceso");
+
+    } else if (accionesAdmin == 2) {
             
             const marcas = ["Samsung", "Philips", "Noblex", "Dell", "Lenovo", "Longvie"];
             
             let marca = prompt("Ingrese la marca que desea buscar");
 
-            alert(marcas.includes(marca));
+            let resultadoBusqueda = marcas.includes(marca);
 
-        } else {
-            if (accionesAdmin == 3) {
+            if (resultadoBusqueda == true) {
+                
+                alert("¡Estás de suerte! ¡La marca que buscás se encuentra en nuestra base de datos!");
+
+            } else {
+
+                alert("Lamentamos decirte que la marca que buscás no se encuentra en nuestra base de datos");
+
+            }
+
+        } else if (accionesAdmin == 3) {
                 
                 function Producto(categoria, marca, modelo, precio) {
                     this.categoria = categoria;
@@ -39,29 +49,22 @@ if (tipoUsuario == "A") {
                 
                 const producto1 = new Producto(categoria1,marca1,modelo1,precio1);
 
-                alert("El producto ingresado es " +producto1); //Este alert no funciona, la intención es que muestre las propiedades del objeto cargado
+                alert("El producto ingresado es " +JSON.stringify(producto1));                
 
-                console.log(producto1); //Por acá sí sale el objeto
-
+            } else if (accionesAdmin == 4) {
                 
-
-            } else {
-                if (accionesAdmin == 4) {
-                    
+                alert("Sección en construcción. ¡Disculpa las molestias! Por favor, refresca el navegador para reiniciar el proceso");
+                
                 } else {
                     alert("Debes elegir la opción escribiedo un número entre 1 y 4. Por favor, inicia el proceso nuevamente.")
-                    
                 }
-                
-            }
-            
-        }
-    
-    } 
-} else {
-    if (tipoUsuario == "C") {
-        alert("¡Hola Cliente! A continuación, selecciona el número correspondiente a la acción que deseas realizar")
+
+} else if (tipoUsuario == "C") {
+
+        alert("¡Hola Cliente! A continuación, selecciona el número correspondiente a la acción que deseas realizar");
+
     } else {
-        alert("Las únicas selecciones posibles son A o C. Por favor, inicia nuevamente el proceso de validación")
+
+        alert("Las únicas selecciones posibles son A o C. Por favor, inicia nuevamente el proceso de validación");
+
     }
-}
